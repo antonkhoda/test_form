a6ddf63d3f48289078e467ad67cd4973cdb5c311
Anton Khoda <anton.khoda@chisw.com>
2022-01-14 17:07:13 +0200

    added summary page

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const closeBtn = document.querySelector('.main-menu__footer-close');
-const menuBlock = document.querySelector('.main-menu');
-
-closeBtn.addEventListener('click', function() {
-    menuBlock.classList.add('main-menu--isclose');
-    menuBlock.textContent = ''
-    console.log(menuBlock);
-})
